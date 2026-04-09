71eb1c56c2e1139f0014ee889105f1c3afcf8d92
AshishMalla115 <ashishmalla3105@gmail.com>
2026-04-09 12:52:36 +0530

    JAVA TNSIF

---
+++ tnsif/src/org/tnsif/capgemini/c2tc/lambdaexpression/LambdaNopara.java
@@ -0,0 +1,13 @@
+package org.tnsif.capgemini.c2tc.lambdaexpression;
+
+interface Sayable{  
+    public String say();  
+}  
+public class LambdaNopara{  
+public static void main(String[] args) {  
+    Sayable s=()->{  
+        return "I have nothing to say.";  
+    };  
+    System.out.println(s.say());  
+}  
+}  
